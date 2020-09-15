7a7acc6eac78b8cb77e116988ed166e1177c9748
Pablovincenzi <71233600+Pablovincenzi@users.noreply.github.com>
2020-09-15 14:40:55 -0300

    Aula02 heranca
    Aula02 heranca

---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,14 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Funcionario2 f1 = new Funcionario2();
+		f1.setSalario(3000.0);
+		System.out.println(f1.getBonificacao());
+		
+		
+		System.out.println(f1.getSalario());
+	}
+
+}
