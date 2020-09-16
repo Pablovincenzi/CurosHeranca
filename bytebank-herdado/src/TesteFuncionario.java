b99be44ac0369b72f97f3515b82e892935aa8a9f
S208 <S208@S0208>
2020-09-16 08:50:14 -0300

    teste nova classe

--- bytebank-herdado/src/TesteFuncionario.java
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -1,26 +1,22 @@
+import java.util.Scanner;
+
+import com.sun.xml.internal.ws.api.pipe.NextAction;
 
 public class TesteFuncionario {
 
 	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
 		// TODO Auto-generated method stub
-		Funcionario2 f1 = new Funcionario2();
-		f1.setSalario(3000.0);
-		System.out.println(f1.getBonificacao());
+		Funcionario f1 = new Funcionario();
+//		f1.setSalario(3000.0);
+			
 		
-		
+		System.out.println("Informe Salario");
+//		double recebe = ler.nextDouble();
+		f1.setSalario(ler.nextDouble());
+//		System.out.println("VARIAVEL"+recebe);
 		System.out.println(f1.getSalario());
-		
-		Gerente g1 = new Gerente();
-		g1.setNome("Pablo Vincenzi");
-		g1.setCpf("00000001");
-		g1.setSalario(3000);
-		
-		System.out.println(g1.getSalario());
-		g1.setSenha(2323);
-		boolean senha = g1.autenticasenha(2323);
-		System.out.println(senha);
-		
-		
+	
 	}
 
 }
