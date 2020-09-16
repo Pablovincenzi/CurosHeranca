d6e8ed548f50fd4c76fc793c1cbcbd97e5205402
pablo <pablo@DESKTOP-PK8T9DO>
2020-09-15 23:41:08 -0300

    aa

--- bytebank-herdado/src/Funcionario.java
+++ bytebank-herdado/src/Funcionario.java
@@ -2,7 +2,7 @@
 public class Funcionario {
 	private String nome;
 	private String cpf;
-	private double salario;
+	protected double salario; //protected Public para os filhos
 	
 	public double getBonificacao() {
 		return this.salario * 0.1;
